1c893acdcfe2c0816fb315a2c70efe03c4ee4573
Hector Lozano <hlozano2@cnm.edu>
2016-10-04 16:15:47 -0600

    Added test page to ensure PhpStorm is setup

---
+++ .idea/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>The Vitamin Store</title>
+</head>
+<body>
+	<h1>
+		Welcome to the Website of The Vitamin Store
+	</h1>
+	<p>This is our first foray onto the World Wide Web.
+	We Are a small company dedicated to the health
+	of our customers through natural remedies.
+	We have a wide range of products. Come in to our store
+	and see our selection</p>
+</body>
+</html>
